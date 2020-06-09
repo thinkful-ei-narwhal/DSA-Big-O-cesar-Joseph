c8858f9b4f2f865aeb0bdab322eab6454b03f4db
Cesar Morales <cesarlenin2@live.com>
2020-06-09 09:54:37 -0700

    finished most 9 out of 11 questions

---
+++ big-O.js
@@ -0,0 +1,33 @@
+// 1. What is the Big O for this?
+// 	1.Constant time O(1)
+// 	2.linear O(n)
+
+// 2. Even or odd
+// 	A:Constant time O(1) because no matter the input the run time will be the same
+
+// 3. Are you here?
+// 	A: Polynomial time O(n^k) because theres a nested for loop
+
+// 4. Doubler
+// 	A:linear O(n) because its a single for loops that time icrease linearly as the array length increases
+
+// 5. Naive search
+// 	A:
+
+// 6. Creating pairs:
+// 	A: Polynomial time O(n^k) because theres a nested for loop
+
+// 7. Compute the sequence
+// 	A:the equation is given a number and it outputs a sequence of numbers (the fibinachi sequence) up to the place of the input. linear O(n)
+
+// 8. An efficient search
+// 	A:Logarithmic time O(log(n)) because it doesnt go through all of the array. it searches in pieces for what its looking for.
+
+// 9. Random element
+// 	A:Constant time O(1) because no matter the input the run time will be the same
+
+// 10. What Am I?
+// 	A: The algorithm is checking if the numbers is an odd number. Exponential time O(2^n)
+
+// 11. Tower of Hanoi
+// 	A:
