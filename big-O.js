646535d020ab631142aa01aa84ba51692a0a0d6f
Cesar Morales <cesarlenin2@live.com>
2020-06-09 11:13:51 -0700

    12.2 question

--- big-O.js
+++ big-O.js
@@ -46,3 +46,25 @@
 // }
 
 // TOH(1,'A','B','C');
+
+// Counting Sheep
+
+function sheepJump(numSheep) {
+  for(let i = numSheep; i >= 0; i--) {
+    if (i === 0) return console.log('All sheep jumped over the fence');
+    console.log(`${i} Another sheep jumped over the fence`);
+  }
+}
+  
+sheepJump(3);
+
+// 12.2 Power Calculator
+
+function powerCalculator(num, exponent){
+  let value=1;
+  for (let i=0; i<exponent; i++){
+    value *=num;
+  }
+  return value;
+}
+console.log(powerCalculator(3,4));
